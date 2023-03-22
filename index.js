9a0efa39276d04c3bcc4bf6136a333f00088722a
StormThorn91 <caitvsistoso@gmail.com>
2023-03-22 18:25:36 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const db = require("./db.json");
+const app = express();
+const port = process.env.PORT || 5500
+
+app.get('/projects', (req, res) => {
+    res.send(db.projects);
+});
+
+app.get('/projects/:id', (req, res) => {
+    const project = db.projects.find(p => p.id === parseInt(req.params.id));
+    if(!project) {
+        res.status(404).send(`Project with id = ${req.params.id} is not found`)
+    }
+    res.status(200).send(project);
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
